025ede2358c5482f70b7418e1b35b236a268885e
Ahmed Basuony <A.Basuony.social@gmail.com>
2026-02-07 18:29:27 +0200

    update axios.js file for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   // https://backend-ecommerce-node.vercel.app/
   baseURL:
-    import.meta.mode === "development"
-      ? "http://localhost:5000/api"
-      : "https://backend-ecommerce-node.vercel.app/api",
+    import.meta.mode === "development" ? "http://localhost:5000/api" : "https://backend-ecommerce-node.vercel.app/api",
   withCredentials: true, // send cookies to the server
 });
 
